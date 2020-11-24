5a09a80a62859dc059006a6501bb898e7ce63ca1
Valeriy V Nepluev <info@nepluev.com>
2020-11-25 00:40:19 +0300

    перед подключением скриптов

---
+++ js/init.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.arrow-left__link'),
+        nextArrow: $('.arrow-right__link')
+  });
+});
