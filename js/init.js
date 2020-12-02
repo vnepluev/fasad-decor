8710a6de4abcf3c69fa0b93fa7639e82e4b07975
Valeriy V Nepluev <info@nepluev.com>
2020-12-02 21:42:44 +0300

    Доделал скрипт слайдера, кнопки заказать звонок, модальное окно не прокручивается

--- js/init.js
+++ js/init.js
@@ -1,21 +1,22 @@
-$(document).ready(function(){
-    // $('.slider').slick({
-    //    slidesToShow: 1,
-    //    slidesToScroll: 1,
-    //    prevArrow: $('.arrow-left'),
-    //    nextArrow: $('.arrow-right')
-    // });
-
-  let btnModal = document.querySelector('#callback-btn');
+$(document).ready(function () {
+  
+  let btnModal = document.querySelectorAll('.btn__link'); // получаем все кнопки заказать звонок
   let btnModal1 = document.querySelector('#callback-btn1');
   let modal = document.querySelector('#modal');
   // Кнопка закрытия модального окна
   let btnClose = document.querySelector('#btn-modal-close'); 
 
   // при клике на кнопку "Заказать обратный звонок"
-  btnModal.addEventListener('click', function () {
-    modal.classList.add('modal-active');
+  btnModal.forEach(function(item){
+    item.addEventListener('click', function () {
+      modal.classList.add('modal-active');
+      document.body.style.overflow = 'hidden'; //убираем прокрутку у окна
+    })
   })
+
+  // btnModal.addEventListener('click', function () {
+  //   modal.classList.add('modal-active');
+  // })
   btnModal1.addEventListener('click', function (e) {
     e.preventDefault();
     modal.classList.add('modal-active');
@@ -24,6 +25,7 @@ $(document).ready(function(){
   // закрытие модального окна "Заказать звонок"
   btnClose.addEventListener('click', function () {
     modal.classList.remove('modal-active');
+    document.body.style.overflow = 'visible';
   })
   $(document).keydown(function(e) {
     if (e.keyCode === 27) {
